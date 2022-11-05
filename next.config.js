4053a1cc4b0e88acb8230e1758f93252944277ae
TJ Draper <tj@buzzingpixel.com>
2022-11-05 14:43:53 -0500

    Update config to route only something.route.ts[x]

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    poweredByHeader: false,
     reactStrictMode: true,
     swcMinify: true,
+    pageExtensions: ['route.ts', 'route.tsx'],
 }
 
 module.exports = nextConfig
