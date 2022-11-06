b5ff6657e4e25538e91a308690d8f5617af5ebed
TJ Draper <tj@buzzingpixel.com>
2022-11-05 21:03:16 -0500

    Update copy on home page

--- pages/index.route.tsx
+++ pages/index.route.tsx
@@ -8,13 +8,16 @@ const Page: NextPage = () => (
     <AppShell>
         <div className="mx-auto mt-2 max-w-7xl px-4 sm:mt-4 sm:px-6">
             <div className="text-center">
-                <h1 className="tracking-tight text-gray-900">
+                <h1 className="mx-auto max-w-2xl tracking-tight text-gray-900">
                     <span className="block font-bold text-4xl sm:text-5xl mb-2">TJ Draper</span>
-                    <span className="block text-tjd-red-500 text-2xl sm:text-3xl">Christian, family man, software engineer, nerd</span>
+                    <span
+                        className="block text-tjd-red-500 text-md sm:text-xl"
+                        dangerouslySetInnerHTML={{ __html: marked.parseInline('Senior Software Engineer. Reformed Presbyterian (baptize yo babies). Crazy CREC nutcase (feed yo babies). Deacon. Conservative libertarian.') }}
+                    />
                 </h1>
                 <p
-                    className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl"
-                    dangerouslySetInnerHTML={{ __html: marked.parseInline('Here you\'ll find an eclectic collection of writings on software, technology, theology, musings, and information about me and my family.') }}
+                    className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-4xl md:text-xl"
+                    dangerouslySetInnerHTML={{ __html: marked.parseInline('On this you\'ll find an eclectic collection of writings on software, technology, theology, musings, and information about me and my family.') }}
                 />
             </div>
         </div>
